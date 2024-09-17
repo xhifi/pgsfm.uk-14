994d0bc4899f52bf1170d2b44de52a9f5cc6cd67
shifi <shifa.newversion@gmail.com>
2024-09-18 02:59:51 +0500

    service card images replaced and fixed for seo

--- src/components/service/ServiceCard.jsx
+++ src/components/service/ServiceCard.jsx
@@ -4,17 +4,17 @@ const ServiceCard = ({ data }) => {
   return data.map((v, i) => {
     return (
       <div className="col mb-4" key={i}>
-        <div className="card h-100 border-0 border-r shadow">
+        <div className="card h-100 border-0 shadow rounded-lg overflow-hidden">
           <div className="image-container d-block">
-            <Image src={v.image} layout="intrinsic" width={1600 / 2} height={1000 / 2} className="card-img-top" alt="..." />
+            <Image src={v.image} className="w-full h-auto" alt={v.heading} />
           </div>
           <div className="card-body">
             <h5 className="card-title text-center">{v.heading}</h5>
             <ul className="list-unstyled text-center">
-              {v.list.map((l, i) => {
+              {v.list.map((item) => {
                 return (
-                  <li className="p-1 bg-main m-1 rounded" key={i}>
-                    {l}
+                  <li className="p-1 bg-main m-1 rounded" key={item}>
+                    {item}
                   </li>
                 );
               })}
